ef651d6b5db676d72b2dd6be986b5acb5fe64f01
HotBurger <154551644+HotBurgi@users.noreply.github.com>
2024-06-02 10:16:59 +0200

    constants

---
+++ Source Code/const.py
@@ -0,0 +1,11 @@
+# RGB color for white
+WHITE = (255, 255, 255)
+
+# Number of columns and rows in the grid
+COLS, ROWS = 160, 160
+
+# Size of each cell in pixels
+CELL = 5
+
+# Width and height of the Pygame window
+WIDTH, HEIGHT = 800, 800
